c4993e3708eacfbfca4237ed4c8328c22c36f754
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-07 02:39:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,7 +16,7 @@
 #     "City Hall" => ["Andrew"]
 #   }
 # }
-require 'pry'
+# require 'pry'
 
 def nyc_pigeon_organizer(data)
   final results = data.each_with_object({}) do |(key, value), final_array|
