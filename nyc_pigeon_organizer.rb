47c0fa328e13306064280b4f4e03f8caf52b1d12
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-07 02:25:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,14 +20,12 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
-    # binding.pry
-  end
-  value.each do |inner_key, names|
-    names.each do |name|
-    binding.pry
+    value.each do |inner_key, names|
+      names.each do |name|
+      end
   end
   final_array
-  binding.pry
+  # binding.pry
 end
 
 
