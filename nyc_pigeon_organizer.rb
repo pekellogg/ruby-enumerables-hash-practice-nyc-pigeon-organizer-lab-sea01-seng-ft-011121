f0e2a40fde7b5c7427d57301fafad12063dd1d26
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-07 02:32:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -25,6 +25,8 @@ def nyc_pigeon_organizer(data)
         if !final_array[name]
           final_array[name] = {}
         end
+        if !final_array[name][key] = []
+        end
       end
   end
   final_array
