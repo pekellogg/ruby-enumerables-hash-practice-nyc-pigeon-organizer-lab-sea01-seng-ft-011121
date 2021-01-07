017000dd6393f3ed23f2431a98fb6e9824f0c75c
Peyton Kellogg <pekellogg@hotmail.com>
2021-01-07 02:19:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,9 +19,11 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
-    final_array[key] = value + 3 # Vincent DeVendra
+  data.each_with_object({}) do |(key, value), final_array| 
+  data.each_with_object({}) do |(key, value), final_array| 
+    final_array[key] = value + 3 
   end
+  final_array
   binding.pry
 end
 
